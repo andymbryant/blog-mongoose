dec7aaf04fcb50a9cb71c2d06d49ac15c83d32f3
Andy Bryant <a.mcdermid.bryant@gmail.com>
2017-11-09 07:44:34 -0800

    done

--- server.js
+++ server.js
@@ -1,43 +1,162 @@
-
-const express = require('express');
-const morgan = require('morgan');
 const bodyParser = require('body-parser');
-const jsonParser = bodyParser.json();
+const express = require('express');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+
+
+
+
+const {PORT, DATABASE_URL} = require('./config');
+const {Post} = require('./models');
 
 const app = express();
+app.use(bodyParser.json());
 
-const blogRouter = require('./blogRouter');
-const {BlogPosts} = require('./models');
 
-app.use(morgan('common'));
 
-app.use(express.static('public'));
+app.get('/blog-posts', (req, res) => {
+  Post
+    .find()
+    // we're limiting because restaurants db has > 25,000
+    // documents, and that's too much to process/return
+    .limit(10)
+    // success callback: for each restaurant we got back, we'll
+    // call the `.apiRepr` instance method we've created in
+    // models.js in order to only expose the data we want the API return.
+    .then(posts => {
+      res.json({
+        posts: posts.map(
+          (post) => post.apiRepr())
+      });
+    })
+    .catch(
+      err => {
+        console.error(err);
+        res.status(500).json({message: 'Internal server error'});
+    });
+});
 
-app.use('/', blogRouter);
+// can also request by ID
+app.get('/blog-posts/:id', (req, res) => {
+  Post
+    // this is a convenience method Mongoose provides for searching
+    // by the object _id property
+    .findById(req.params.id)
+    .then(post =>res.json(post.apiRepr()))
+    .catch(err => {
+      console.error(err);
+        res.status(500).json({message: 'Internal server error'})
+    });
+});
+
+
+app.post('/blog-posts', (req, res) => {
+
+  const requiredFields = ['name', 'borough', 'cuisine'];
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+
+  Post
+    .create({
+      name: req.body.name,
+      borough: req.body.borough,
+      cuisine: req.body.cuisine,
+      grades: req.body.grades,
+      address: req.body.address})
+    .then(
+      restaurant => res.status(201).json(restaurant.apiRepr()))
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({message: 'Internal server error'});
+    });
+});
+
+
+app.put('/restaurants/:id', (req, res) => {
+  // ensure that the id in the request path and the one in request body match
+  if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+    const message = (
+      `Request path id (${req.params.id}) and request body id ` +
+      `(${req.body.id}) must match`);
+    console.error(message);
+    return res.status(400).json({message: message});
+  }
+
+  // we only support a subset of fields being updateable.
+  // if the user sent over any of the updatableFields, we udpate those values
+  // in document
+  const toUpdate = {};
+  const updateableFields = ['name', 'borough', 'cuisine', 'address'];
+
+  updateableFields.forEach(field => {
+    if (field in req.body) {
+      toUpdate[field] = req.body[field];
+    }
+  });
+
+  Restaurant
+    // all key/value pairs in toUpdate will be updated -- that's what `$set` does
+    .findByIdAndUpdate(req.params.id, {$set: toUpdate})
+    .then(restaurant => res.status(204).end())
+    .catch(err => res.status(500).json({message: 'Internal server error'}));
+});
+
+app.delete('/restaurants/:id', (req, res) => {
+  Restaurant
+    .findByIdAndRemove(req.params.id)
+    .then(restaurant => res.status(204).end())
+    .catch(err => res.status(500).json({message: 'Internal server error'}));
+});
+
+
+app.use('*', function(req, res) {
+  res.status(404).json({message: 'Not Found'});
+});
 
 let server;
-function runServer() {
-  const port = process.env.PORT || 8080;
+
+// this function connects to our database, then starts the server
+function runServer(databaseUrl=DATABASE_URL, port=PORT) {
+
   return new Promise((resolve, reject) => {
-    server = app.listen(port, () => {
-      console.log(`Your app is listening on port ${port}`);
-      resolve(server);
-    }).on('error', err => {
-      reject(err)
+    mongoose.connect(databaseUrl, err => {
+      if (err) {
+        return reject(err);
+      }
+      server = app.listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+        resolve();
+      })
+      .on('error', err => {
+        mongoose.disconnect();
+        reject(err);
+      });
     });
   });
 }
 
+
+// this function closes the server, and returns a promise. we'll
+// use it in our integration tests later.
 function closeServer() {
-  return new Promise((resolve, reject) => {
-    console.log('Closing server');
-    server.close(err => {
-      if (err) {
-        reject(err);
-        return;
-      }
-      resolve();
-    });
+  return mongoose.disconnect().then(() => {
+     return new Promise((resolve, reject) => {
+       console.log('Closing server');
+       server.close(err => {
+           if (err) {
+               return reject(err);
+           }
+           resolve();
+       });
+     });
   });
 }
 
